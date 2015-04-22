69a945531bad3bc264758b130f700df79b27b8ca
Unplugged Coder <unpluggedcoder@163.com>
2015-04-22 10:51:34 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+
+setup(name='taobaoip',
+      version='0.9',
+      description='A tool for get ip information from http://ip.taobao.com',
+      author='Unplugged Coder',
+      author_email='unpluggedcoder@163.com',
+      url='https://github.com/Ghostist/taobaoip',
+     )
